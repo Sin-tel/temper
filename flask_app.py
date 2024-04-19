91bd36b822fca9ffbbb0ce34b75b361317c306fa
Sintel <sintel.inquiries@gmail.com>
2024-04-19 18:08:07 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,9 @@
 import traceback
 import flask as f
+import os
+
+# workaround for git import errors
+os.environ["GIT_PYTHON_REFRESH"] = "quiet"
 import git
 from timeout import time_limit, TimeoutException
 from info import *
